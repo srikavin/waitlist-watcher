7e4a7ca33364424792ab03f617e48b90ad6f3148
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2022-08-23 16:52:28 -0400

    update event back populating scripts

---
+++ util/move_to_cloud_storage.ts
@@ -0,0 +1,106 @@
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+import {applyPatch} from "fast-json-patch/commonjs/core";
+import {CourseDataCourses} from "./types";
+
+import {Storage} from "@google-cloud/storage";
+
+const storage = new Storage();
+const historical_bucket = storage.bucket('waitlist-watcher-historical-data')
+
+const BUCKET_SNAPSHOT_PREFIX = (department: string) => `snapshots/${department}/`
+
+initializeApp();
+
+const db = getFirestore();
+db.settings({ignoreUndefinedProperties: true})
+
+const course_data = db.collection('course_data');
+
+const fetch_historical_data = async (department: string) => {
+    let departmentRef = course_data.doc(department);
+    let historicalRef = departmentRef.collection("historical");
+
+    return await historicalRef.listDocuments();
+}
+
+type CourseDataHistoricalEntry = {
+    type: 'full',
+    contents: CourseDataCourses,
+} | {
+    type: 'diff',
+    basedOn: string,
+    diff: any,
+}
+
+const reconstructData = (prev: object | null, current: any): CourseDataCourses => {
+    console.log(current.type)
+
+    if (current.type === 'full') {
+        return current.contents as CourseDataCourses;
+    } else {
+        if (current.diff.length > 1 && current.diff[1].op === 'replace' && current.diff[1].path === "") {
+            console.log("Replaced entire object")
+            return current.diff[1].value;
+        }
+
+        return applyPatch(prev, current.diff, true, false).newDocument as CourseDataCourses;
+    }
+}
+
+const normalizeData = (data: any) => {
+    if (!Array.isArray(data)) {
+        return data;
+    }
+
+    return Object.fromEntries((data as any).map((e: any) => {
+        return [e.course, {...e, sections: Object.fromEntries(e.sections.map((s: any) => [s.section, s]))}]
+    }))
+}
+
+const runForDepartment = async (department: string) => {
+    const data = await fetch_historical_data(department)
+    const downloads = data.map(e => e.get());
+    const uploads = [];
+
+    let prev: object | null = {};
+    for (let i = 0; i < data.length; ++i) {
+        const curData = (await downloads[i]).data() as CourseDataHistoricalEntry;
+
+        let cur: CourseDataCourses;
+        try {
+            cur = reconstructData(prev, curData);
+        } catch (e) {
+            // skip until next full snapshot
+            console.log(department, "skipped", data[i].id);
+            prev = null;
+            continue;
+        }
+
+        let normalizedCur = normalizeData(cur);
+        uploads.push(historical_bucket.file(BUCKET_SNAPSHOT_PREFIX(department) + data[i].id + '.json').save(JSON.stringify(normalizedCur)));
+        console.log(department, "Uploaded snapshot", i, data[i].id);
+
+        prev = cur;
+
+    }
+    await Promise.all(uploads);
+
+    let deletions = []
+    for (let i = 0; i < data.length; ++i) {
+        console.log(department, "Deleted", i, data[i].id);
+        deletions.push(data[i].delete())
+    }
+
+    await Promise.all(deletions);
+}
+
+(async () => {
+    for (const department of ["AASP", "AAST", "AGNR", "AGST", "AMSC", "AMST", "ANSC", "ANTH", "AOSC", "ARAB", "ARCH", "AREC", "ARHU", "ARMY", "ARSC", "ARTH", "ARTT", "ASTR", "BCHM", "BIOE", "BIOI", "BIOL", "BIOM", "BIPH", "BISI", "BMGT", "BMSO", "BSCI", "BSCV", "BSGC", "BSOS", "BSST", "BUAC", "BUDT", "BUFN", "BULM", "BUMK", "BUSI", "BUSM", "BUSO", "CBMG", "CCJS", "CHBE", "CHEM", "CHIN", "CHPH", "CHSE", "CINE", "CLAS", "CLFS", "CMLT", "CMSC", "COMM", "CPBE", "CPET", "CPGH", "CPJT", "CPMS", "CPPL", "CPSA", "CPSD", "CPSF", "CPSG", "CPSN", "CPSP", "CPSS", "DANC", "DATA", "ECON", "EDCP", "EDHD", "EDHI", "EDMS", "EDSP", "EDUC", "ENAE", "ENBC", "ENCE", "ENCO", "ENEB", "ENEE", "ENES", "ENFP", "ENGL", "ENMA", "ENME", "ENPM", "ENRE", "ENSE", "ENSP", "ENST", "ENTM", "ENTS", "EPIB", "FGSM", "FIRE", "FMSC", "FREN", "GEMS", "GEOG", "GEOL", "GERM", "GREK", "GVPT", "HACS", "HDCC", "HEBR", "HESI", "HESP", "HGLO", "HHUM", "HISP", "HIST", "HLSA", "HLSC", "HLTH", "HNUH", "HONR", "IDEA", "IMDM", "IMMR", "INAG", "INFM", "INST", "ISRL", "ITAL", "JAPN", "JOUR", "JWST", "KNES", "KORA", "LACS", "LARC", "LATN", "LBSC", "LGBT", "LING", "MATH", "MEES", "MIEH", "MITH", "MLAW", "MLSC", "MOCB", "MSML", "MUED", "MUSC", "NACS", "NAVY", "NEUR", "NFSC", "NIAS", "PEER", "PERS", "PHIL", "PHPE", "PHSC", "PHYS", "PLCY", "PLSC", "PORT", "PSYC", "RDEV", "RELS", "RUSS", "SLAA", "SLLC", "SMLP", "SOCY", "SPAN", "SPHL", "STAT", "SURV", "TDPS", "THET", "TLPL", "TLTC", "UMEI", "UNIV", "URSP", "USLT", "VMSC", "WGSS", "WMST"]) {
+        console.log(department)
+        await runForDepartment(department);
+    }
+})();
+
+export default {};
+
--- util/populate_historical_events.ts
+++ util/populate_historical_events.ts
@@ -1,11 +1,11 @@
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
-import {applyPatch} from "fast-json-patch/commonjs/core";
-import {generateEvents} from "shared/generate_events";
-import {CourseDataCourses} from "shared/types";
-import * as crypto from 'crypto';
+// @ts-ignore
+import {generateEvents} from "../scraper/generate_events";
+import {CourseDataCourses} from "./types";
 
-const {Storage} = require('@google-cloud/storage');
+import {Storage} from "@google-cloud/storage";
+
 const storage = new Storage();
 const historical_bucket = storage.bucket('waitlist-watcher-historical-data')
 
@@ -17,79 +17,28 @@ initializeApp();
 const db = getFirestore();
 db.settings({ignoreUndefinedProperties: true})
 
-const course_data = db.collection('course_data');
 const events_collection = db.collection("events");
 
-const fetch_historical_data = async (department: string) => {
-    let departmentRef = course_data.doc(department);
-    let historicalRef = departmentRef.collection("historical");
-
-    return await historicalRef.listDocuments();
-}
-
-type CourseDataHistoricalEntry = {
-    type: 'full',
-    contents: CourseDataCourses,
-} | {
-    type: 'diff',
-    basedOn: string,
-    diff: any,
-}
-
-const reconstructData = (prev: object | null, current: any): CourseDataCourses => {
-    console.log(current.type)
-
-
-    if (current.type === 'full') {
-        return current.contents as CourseDataCourses;
-    } else {
-        if (current.diff.length > 1 && current.diff[1].op === 'replace' && current.diff[1].path === "") {
-            console.log("Replaced entire object")
-            return current.diff[1].value;
-        }
-
-        return applyPatch(prev, current.diff, true, false).newDocument as CourseDataCourses;
-    }
-}
-
-const normalizeData = (data: any) => {
-    if (!Array.isArray(data)) {
-        return data;
-    }
-
-    return Object.fromEntries((data as any).map((e: any) => {
-        return [e.course, {...e, sections: Object.fromEntries(e.sections.map((s: any) => [s.section, s]))}]
-    }))
-}
-
 const runForDepartment = async (department: string) => {
-    const data = await fetch_historical_data(department);
     const uploads = [];
 
-    let prev: object | null = {};
-    for (let i = 0; i < data.length; ++i) {
+    const [files] = await historical_bucket.getFiles({
+        prefix: `${BUCKET_SNAPSHOT_PREFIX(department)}`
+    });
 
-        const curData = (await data[i].get()).data() as CourseDataHistoricalEntry;
+    let prev: object = {};
 
-        let cur: CourseDataCourses;
-        try {
-            cur = reconstructData(prev, curData);
-        } catch (e) {
-            // skip until next full snapshot
-            console.log("skipped", data[i].id);
-            prev = null;
-            continue;
-        }
+    for (let i = 0; i < files.length; ++i) {
+        const cur = JSON.parse((await files[i].download())[0].toString()) as CourseDataCourses;
 
-        let normalizedCur = normalizeData(cur);
-        uploads.push(historical_bucket.file(BUCKET_SNAPSHOT_PREFIX(department) + data[i].id + '.json').save(JSON.stringify(normalizedCur)));
-        console.log("Uploaded snapshot", i, data[i].id);
+        const parts = files[i].name.split('/');
+        const date = parts[parts.length - 1].substring(0, parts[parts.length - 1].length - ".json".length);
+
+        console.log(i, files[i].name)
 
         if (prev !== null) {
-            let normalizedPrev = normalizeData(prev);
-
-            const events = generateEvents(crypto, normalizedPrev, normalizedCur, data[i].id);
-            uploads.push(historical_bucket.file(BUCKET_EVENTS_PREFIX(department) + data[i].id + '.json').save(JSON.stringify(events)));
+            const events = generateEvents(prev, cur, date);
+            uploads.push(historical_bucket.file(BUCKET_EVENTS_PREFIX(department) + date + '.json').save(JSON.stringify(events)));
 
             for (let event of events) {
                 if (!event.course) {
@@ -100,30 +49,36 @@ const runForDepartment = async (department: string) => {
                 // @ts-ignore
                 let docname = event.course + (event.section ? '-' + event.section : '')
 
+                if (event.type === 'section_added' || event.type === 'section_removed') {
+                    uploads.push(events_collection.doc(event.course).set({
+                        events: {
+                            [event.id]: event
+                        }
+                    }, {merge: true}));
+                }
+
                 uploads.push(events_collection.doc(docname).set({
                     events: {
                         [event.id]: event
                     }
                 }, {merge: true}));
             }
-            console.log("Uploaded events", i, data[i].id, events.length);
+            console.log("Uploaded events", i, date, events.length);
         }
 
         prev = cur;
-
     }
+
     await Promise.all(uploads);
-
-    let deletions = []
-    for (let i = 0; i < data.length; ++i) {
-        deletions.push(data[i].delete())
-        console.log("deleting", data[i].id);
-    }
-
-    await Promise.all(deletions);
 }
 
-runForDepartment("AASP");
+(async () => {
+    let departments = ["AASP", "AAST", "AGNR", "AGST", "AMSC", "AMST", "ANSC", "ANTH", "AOSC", "ARAB", "ARCH", "AREC", "ARHU", "ARMY", "ARSC", "ARTH", "ARTT", "ASTR", "BCHM", "BIOE", "BIOI", "BIOL", "BIOM", "BIPH", "BISI", "BMGT", "BMSO", "BSCI", "BSCV", "BSGC", "BSOS", "BSST", "BUAC", "BUDT", "BUFN", "BULM", "BUMK", "BUSI", "BUSM", "BUSO", "CBMG", "CCJS", "CHBE", "CHEM", "CHIN", "CHPH", "CHSE", "CINE", "CLAS", "CLFS", "CMLT", "CMSC", "COMM", "CPBE", "CPET", "CPGH", "CPJT", "CPMS", "CPPL", "CPSA", "CPSD", "CPSF", "CPSG", "CPSN", "CPSP", "CPSS", "DANC", "DATA", "ECON", "EDCP", "EDHD", "EDHI", "EDMS", "EDSP", "EDUC", "ENAE", "ENBC", "ENCE", "ENCO", "ENEB", "ENEE", "ENES", "ENFP", "ENGL", "ENMA", "ENME", "ENPM", "ENRE", "ENSE", "ENSP", "ENST", "ENTM", "ENTS", "EPIB", "FGSM", "FIRE", "FMSC", "FREN", "GEMS", "GEOG", "GEOL", "GERM", "GREK", "GVPT", "HACS", "HDCC", "HEBR", "HESI", "HESP", "HGLO", "HHUM", "HISP", "HIST", "HLSA", "HLSC", "HLTH", "HNUH", "HONR", "IDEA", "IMDM", "IMMR", "INAG", "INFM", "INST", "ISRL", "ITAL", "JAPN", "JOUR", "JWST", "KNES", "KORA", "LACS", "LARC", "LATN", "LBSC", "LGBT", "LING", "MATH", "MEES", "MIEH", "MITH", "MLAW", "MLSC", "MOCB", "MSML", "MUED", "MUSC", "NACS", "NAVY", "NEUR", "NFSC", "NIAS", "PEER", "PERS", "PHIL", "PHPE", "PHSC", "PHYS", "PLCY", "PLSC", "PORT", "PSYC", "RDEV", "RELS", "RUSS", "SLAA", "SLLC", "SMLP", "SOCY", "SPAN", "SPHL", "STAT", "SURV", "TDPS", "THET", "TLPL", "TLTC", "UMEI", "UNIV", "URSP", "USLT", "VMSC", "WGSS", "WMST"];
+    while (departments.length > 0) {
+        await Promise.all(departments.splice(0, 5).map(runForDepartment))
+        console.log(departments.length)
+    }
+})();
 
 export default {};
 
