edfd5fd3dd92c3f953277b11bbb5c2e12de11c02
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2022-08-25 15:20:32 -0400

    fix issues with search

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,5 +1,6 @@
 import {Autocomplete, TextInput} from "evergreen-ui"
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 let courses: string[] = [];
 fetch("https://waitlist-watcher.uk.r.appspot.com/courses").then(e => e.json()).then((e) => {
@@ -9,11 +10,20 @@ fetch("https://waitlist-watcher.uk.r.appspot.com/courses").then(e => e.json()).t
 export function Search() {
     const navigate = useNavigate();
 
+    const [items, setItems] = useState<string[]>([]);
+
+    useEffect(() => {
+        setItems(courses);
+    }, [courses])
+
     return (
         <Autocomplete
             title="Courses"
-            onChange={changedItem => navigate('/history/' + changedItem)}
-            items={courses}
+            onChange={changedItem => {
+                if (changedItem)
+                    navigate('/history/' + changedItem)
+            }}
+            items={items}
         >
             {subProps => {
                 const {getInputProps, getRef} = subProps
