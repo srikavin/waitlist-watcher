f2cfab62556fc38f3cbe8e7045d1ba6921edf521
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2022-11-08 03:41:31 -0500

    update sw registration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,7 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 
 export const notifWorker = navigator.serviceWorker.register("/sw.js")
-    .then(registration => registration.update())
-    .catch(console.error);
+    .then(registration => registration.update().then(() => registration));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
