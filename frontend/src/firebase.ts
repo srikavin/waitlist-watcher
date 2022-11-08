ee3be9294a53c80083282bd85b48b60a4c1e9097
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2022-11-08 03:37:52 -0500

    update firebase config

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,16 +3,18 @@ import {initializeApp} from "firebase/app";
 import {browserLocalPersistence, getAuth, setPersistence} from "firebase/auth";
 import {getDatabase} from "firebase/database";
 import {getFunctions, httpsCallable} from "firebase/functions";
+import {getAnalytics} from "firebase/analytics";
 
 
 const firebaseConfig = {
     apiKey: "AIzaSyCEOk5uM6dXkaedaGKrayqBGEjHz818hOY",
     authDomain: "waitlist-watcher.firebaseapp.com",
+    databaseURL: "https://waitlist-watcher-default-rtdb.firebaseio.com",
     projectId: "waitlist-watcher",
     storageBucket: "waitlist-watcher.appspot.com",
     messagingSenderId: "315630446423",
     appId: "1:315630446423:web:f5ba6e6dc335eeac6a0012",
-    databaseURL: "https://waitlist-watcher-default-rtdb.firebaseio.com"
+    measurementId: "G-ZQYQSYHS90"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
@@ -20,9 +22,9 @@ export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
 export const realtime_db = getDatabase(firebaseApp);
 export const functions = getFunctions(firebaseApp, "us-east4");
+getAnalytics(firebaseApp);
 
 export const testNotifyFunction = httpsCallable(functions, 'test_notification');
-
 
 setPersistence(auth, browserLocalPersistence);
 enableIndexedDbPersistence(db).catch(console.error);
