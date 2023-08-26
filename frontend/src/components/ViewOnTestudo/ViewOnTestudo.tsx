25f990730473903edf43711877893f4e6916c473
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2023-08-26 18:07:03 -0500

    fix view on testudo link

--- frontend/src/components/ViewOnTestudo/ViewOnTestudo.tsx
+++ frontend/src/components/ViewOnTestudo/ViewOnTestudo.tsx
@@ -4,7 +4,7 @@ export function ViewOnTestudo(props: { course: string, section?: string }) {
     const {semester} = useSemesterContext();
     const {course, section} = props;
 
-    const url = `https://app.testudo.umd.edu/soc/search?courseId=${course}&sectionId=${section ?? ''}&termId=${semester}&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on`;
+    const url = `https://app.testudo.umd.edu/soc/search?courseId=${course}&sectionId=${section ?? ''}&termId=${semester.id}&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on`;
 
     return <a href={url} target="_blank">View on Testudo</a>
 }
