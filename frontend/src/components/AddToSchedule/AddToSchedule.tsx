199b2ecd28f49db770d16b160148fa11cd71b3e8
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2023-08-26 18:07:28 -0500

    fix add to schedule link

--- frontend/src/components/AddToSchedule/AddToSchedule.tsx
+++ frontend/src/components/AddToSchedule/AddToSchedule.tsx
@@ -4,7 +4,7 @@ export function AddToSchedule(props: { course: string, section: string }) {
     const {semester} = useSemesterContext();
     const {course, section} = props;
 
-    const url = `https://app.testudo.umd.edu/main/dropAdd?venusTermId=${semester}&crslist=${course}_/${section}`;
+    const url = `https://app.testudo.umd.edu/main/dropAdd?venusTermId=${semester.id}&crslist=${course}_/${section}`;
 
     return <a href={url} target="_blank">Add to Schedule</a>
 }
