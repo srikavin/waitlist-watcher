a7bdaa17bef237aa80210b9430d584b9afae5133
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2026-02-20 22:50:48 -0600

    course listing

--- frontend/src/components/CoursePrefixListing/CoursePrefixListing.tsx
+++ frontend/src/components/CoursePrefixListing/CoursePrefixListing.tsx
@@ -1,10 +1,20 @@
 import {Link} from "evergreen-ui";
-
-import prefixes from "./prefixes.json";
 import {useNavigate} from "react-router-dom";
+import {useMemo} from "react";
+import {useSemesterContext} from "@/frontend/src/context/SemesterContext";
 
 export function CoursePrefixListing() {
     const navigate = useNavigate();
+    const {courseListing} = useSemesterContext();
+    const prefixes = useMemo(() => {
+        const result = new Set<string>();
+        for (const item of courseListing) {
+            const match = item.match(/^[A-Z]{3,8}/);
+            if (!match) continue;
+            result.add(match[0]);
+        }
+        return Array.from(result).sort((a, b) => a.localeCompare(b));
+    }, [courseListing]);
 
     return (
         <div style={{columnCount: 10, marginTop: 8}}>
